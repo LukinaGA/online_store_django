8f43d3d873a3c216981a5c9c4406ce61d44271ba
Galina Lukina <lukina.galina11@gmail.com>
2024-11-10 14:40:03 +0400

    add creation and deletion of blogs

--- blogs/admin.py
+++ blogs/admin.py
@@ -5,6 +5,6 @@ from blogs.models import Blog
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'publication_sign')
-    list_filter = ('publication_sign',)
-    search_fields = ('title', 'article')
+    list_display = ("title", "created_at", "publication_sign")
+    list_filter = ("publication_sign",)
+    search_fields = ("title", "article")
--- blogs/urls.py
+++ blogs/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 
 from blogs.apps import BlogsConfig
-from blogs.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
-
+from blogs.views import BlogCreateView, BlogDeleteView, BlogDetailView, BlogListView, BlogUpdateView
 
 app_name = BlogsConfig.name
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='blog_list'),
-    path('blogs/new/', BlogCreateView.as_view(), name='blog_create'),
-    path('blogs/blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
-    path('blogs/update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
-    path('blogs/delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
+    path("", BlogListView.as_view(), name="blog_list"),
+    path("new/", BlogCreateView.as_view(), name="blog_create"),
+    path("blog/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("update/<int:pk>/", BlogUpdateView.as_view(), name="blog_update"),
+    path("delete/<int:pk>/", BlogDeleteView.as_view(), name="blog_delete"),
 ]
