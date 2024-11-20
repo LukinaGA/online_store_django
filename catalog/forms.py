0fa7b217a70b8fa6fc39fef9bac2d8aa735978f1
Galina Lukina <lukina.galina11@gmail.com>
2024-11-20 23:46:16 +0400

    add styling to Product forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,6 +8,14 @@ class ProductForm(forms.ModelForm):
         model = Product
         exclude = ('created_at', 'updated_at')
 
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['image'].widget.attrs.update({'class': 'form-control'})
+        self.fields['category'].widget.attrs.update({'class': 'form-control'})
+        self.fields['price'].widget.attrs.update({'class': 'form-control'})
+
     def clean_name(self):
         forbidden_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "дёшево", "бесплатно", "обман", "полиция",
                            "радар"]
