5531667c6e1475f9929800e9906955f45a352531
Galina Lukina <lukina.galina11@gmail.com>
2024-11-28 21:33:33 +0400

    add command for create superuser

--- config/settings.py
+++ config/settings.py
@@ -101,3 +101,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_REDIRECT_URL = 'catalog:products_list'
+LOGOUT_REDIRECT_URL = 'catalog:products_list'
